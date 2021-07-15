0dd3174e36a34944bef57b6f760cc10462df01cc
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-07-15 16:56:38 -0400

    comments initiated

--- screens/VideoScreen/VideoScreen.tsx
+++ screens/VideoScreen/VideoScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import video from "../../assets/data/video.json";
 import { styles } from "./styles";
@@ -16,33 +16,70 @@ const VideoScreen = () => {
         <Text style={styles.tags}>{video.tags}</Text>
         <Text style={styles.title}>{video.title}</Text>
         <Text style={styles.subTitle}>
-          {video.user.name} · {} · {video.createdAt}
+          {video.views} views · {video.createdAt}
         </Text>
       </View>
       <View style={styles.actionListContainer}>
-        <View style={styles.actionListItem}>
-          <AntDesign name="like2" size={28} color="grey" />
-          <Text style={styles.iconText}>2323</Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={styles.actionListItem}>
+            <AntDesign name="like2" size={24} color="grey" />
+            <Text style={styles.iconText}>{video.likes}</Text>
+          </View>
+          <View style={styles.actionListItem}>
+            <AntDesign name="dislike2" size={24} color="grey" />
+            <Text style={styles.iconText}>{video.dislikes}</Text>
+          </View>
+          <View style={styles.actionListItem}>
+            <Ionicons name="chatbubbles-outline" size={24} color="grey" />
+            <Text style={styles.iconText}>Live Chat</Text>
+          </View>
+          <View style={styles.actionListItem}>
+            <MaterialCommunityIcons name="share" size={24} color="grey" />
+            <Text style={styles.iconText}>Share</Text>
+          </View>
+          <View style={styles.actionListItem}>
+            <MaterialCommunityIcons name="download" size={24} color="grey" />
+            <Text style={styles.iconText}>Download</Text>
+          </View>
+          <View style={styles.actionListItem}>
+            <MaterialIcons name="save" size={24} color="grey" />
+            <Text style={styles.iconText}>Save</Text>
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.userContainer}>
+        <Image source={{ uri: video.user.image }} style={styles.avatar} />
+        <View style={{ flex: 1, paddingHorizontal: 15 }}>
+          <Text style={styles.userTitle}>{video.user.name}</Text>
+          <Text style={{ color: "lightgrey", fontSize: 12 }}>
+            {video.user.subscribers} subscribers
+          </Text>
         </View>
-        <View style={styles.actionListItem}>
-          <AntDesign name="dislike2" size={28} color="grey" />
-          <Text style={styles.iconText}>23</Text>
-        </View>
-        <View style={styles.actionListItem}>
-          <Ionicons name="chatbubbles-outline" size={28} color="grey" />
-          <Text style={styles.iconText}>Live Chat</Text>
-        </View>
-        <View style={styles.actionListItem}>
-          <MaterialCommunityIcons name="share" size={28} color="grey" />
-          <Text style={styles.iconText}>Share</Text>
-        </View>
-        <View style={styles.actionListItem}>
-          <MaterialCommunityIcons name="download" size={28} color="grey" />
-          <Text style={styles.iconText}>Download</Text>
-        </View>
-        <View style={styles.actionListItem}>
-          <MaterialIcons name="save" size={28} color="grey" />
-          <Text style={styles.iconText}>Save</Text>
+        <Text style={styles.subscribeBtn}>Subscribe</Text>
+        <MaterialCommunityIcons name="bell-outline" size={24} color="grey" />
+      </View>
+      {/* Comments */}
+
+      <View>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 14,
+            marginTop: 10,
+            marginRight: 10,
+          }}
+        >
+          Comments
+          <Text style={{ color: "lightgrey" }}> 333</Text>
+        </Text>
+        <View style={styles.commentItem}>
+          <Image
+            style={{ height: 25, width: 25, borderRadius: 13, marginRight: 10 }}
+            source={{ uri: video.user.image }}
+          />
+          <Text style={{ color: "white", fontSize: 12 }}>
+            Some comments here
+          </Text>
         </View>
       </View>
     </SafeAreaView>
--- screens/VideoScreen/styles.ts
+++ screens/VideoScreen/styles.ts
@@ -28,15 +28,49 @@ export const styles = StyleSheet.create({
   actionListContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    paddingHorizontal: 10,
+    paddingHorizontal: 5,
   },
   actionListItem: {
     padding: 10,
+    paddingHorizontal: 20,
     height: 72,
     justifyContent: "space-between",
     alignItems: "center",
   },
   iconText: {
     color: "grey",
+  },
+  userContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "rgba(255,255,255,0.2)",
+    paddingVertical: 10,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  userTitle: {
+    color: "white",
+    fontSize: 16,
+  },
+  subscribeBtn: {
+    textTransform: "uppercase",
+    color: "grey",
+    fontSize: 16,
+    fontWeight: "600",
+    paddingHorizontal: 20,
+  },
+  commentItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+
+    borderBottomWidth: 1,
+    borderColor: "rgba(255,255,255,0.2)",
   },
 });
